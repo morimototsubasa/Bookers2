d16021d71d7f29e29170194e1ec9f9e5c9d36c43
morimototsubasa <119485681+morimototsubasa@users.noreply.github.com>
2022-12-16 09:58:04 +0000

    kadai3

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  # get 'users/show'
+  # get 'users/edit'
   devise_for :users
+  resources :users
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  resources :books
+  # get 'homes/top'
+  root to: 'homes#top'
+
+  get "/homes/about" => "homes#about" ,as: "about"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
